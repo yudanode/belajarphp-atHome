37e9e27724f832bf46eeec1fd4d1814bf47725c5
Youpiiiiiiiiiiii <yudannuralif@gmail.com>
2026-05-10 14:43:29 +0700

    feat: remove success message from database connection script

--- tugas_crud/koneksi.php
+++ tugas_crud/koneksi.php
@@ -3,8 +3,6 @@ $conn = mysqli_connect("localhost", "root","","db_mahasiswa");
 
 if(!$conn){
     die("Gagal tersambung!, error" . mysqli_connect_error());
-}else{
-    echo "berhasil tersambung";
 }
 
 
