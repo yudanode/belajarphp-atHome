2a33ddbbdc816894a8f895fd48f1adb04a02dfe0
Youpiiiiiiiiiiii <yudannuralif@gmail.com>
2026-05-10 14:43:23 +0700

    feat: refactor insert functionality to process input data and handle errors

--- tugas_crud/insert.php
+++ tugas_crud/insert.php
@@ -1,16 +1,22 @@
 <?php
 include "koneksi.php";
 if (isset($_POST['simpan'])){
-    $result = mysqli_query($conn, "INSERT INTO mahasiswa (nim, nama, prodi, email) VALUES ('$_POST[nim]','$_POST[nama]','$_POST[prodi]','$_POST[email]')");
+    $nim_lengkap = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $prodi = $_POST['prodi'];
+    $email = $_POST['email'];
 
-    
+    $nim_potong = substr($nim_lengkap, -3);
 
+    $querry = mysqli_query($conn, "INSERT INTO mahasiswa (nim, nama, prodi, email) VALUES ('$nim_potong', '$nama', '$prodi', '$email')");
 
-    if(!$result){
-        die("Error: " . mysqli_error($conn));
+    if($querry){
+        echo "<script>alert('Data berhasil di simpan!'); document.location.href = 'index.php';</script>";
+    }else{
+        echo "<script>alert('Data gagal untuk di simpan, nim duplikat!'); document.location.href = 'insert.php';</script>";
     }
 
-    header("Location: index.php");
+
     
 }
 
